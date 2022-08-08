c6e6a860e95ba80c2c8e40dbcb87f0ceae67fe5b
Matthew Jovenal <matthewjovenal1@outlook.com>
2022-08-08 10:59:42 -0700

    Facts buttons change dynamically and update the photo and content text. small bug when changing planets

--- frontend/src/Components/PlanetFactsComponent.jsx
+++ frontend/src/Components/PlanetFactsComponent.jsx
@@ -1,28 +1,59 @@
-const PlanetFacts = ({ planet }) => {
-  
+import { useState, useEffect } from "react";
 
+const PlanetFacts = ({ planet }) => {
+
+  const [text, setText] = useState('');
+  const [image, setImage] = useState('');
+  const [link, setLink] = useState('');
+  const [active, setActive] = useState(0);
+
+  const handleState = () => {
+    switch(active){
+      case 1:
+        setText(planet.structureContent);
+        setLink(planet.structureSource);
+        setImage(planet.imagesInternal);
+        break;
+      case 2:
+        setText(planet.geologyContent);
+        setLink(planet.geologySource);
+        setImage(planet.imagesGeology);
+        break;
+      default:
+        setText(planet.overviewContent);
+        setLink(planet.overviewSource);
+        setImage(planet.imagesPlanet);
+        break;
+    }
+  }
+
+  useEffect(() => {
+    handleState();
+  }, [active])
+
+  
   return (
     <div className="animate__animated animate__zoomInUp">
       <div className="planetFacts">
-        <object type="image/svg+xml" data={`.${planet.imagesPlanet}`}></object>
+        <object type="image/svg+xml" data={`.${image}`}></object>
         <div className="planetFactsContent">
           <h1>{planet.name}</h1>
-          <p>{planet.overviewContent}</p>
+          <p>{text}</p>
           <p className="planetFactsLink">
             Source:{" "}
-            <a href="" target={"_blank"}>
+            <a href={link} target={"_blank"}>
               Wikipedia
             </a>
           </p>
-          <button className="planetFactsButton" id="active">
+          <button className="planetFactsButton" id={active === 0 ? "active" : ""} onClick={() => setActive(0)} >
             <span className="spanNum">01</span>
             <span>Overview</span>
           </button>
-          <button className="planetFactsButton">
+          <button className="planetFactsButton" onClick={() => setActive(1)} id={active === 1 ? "active" : ""}>
             <span className="spanNum">02</span>
             <span>Internal Structure</span>
           </button>
-          <button className="planetFactsButton">
+          <button className="planetFactsButton" onClick={() => setActive(2)} id={active === 2 ? "active" : ""}>
             <span className="spanNum">03</span>
             <span>Surface Geology</span>
           </button>
